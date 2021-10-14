8102d8317f5f16dd0698d11a51aab3e00da81836
godongs <asd236678@gmail.com>
2021-10-14 19:17:53 +0900

    init spring

---
+++ src/test/java/kr/couchcoding/herokuactions/HerokuActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.couchcoding.herokuactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
